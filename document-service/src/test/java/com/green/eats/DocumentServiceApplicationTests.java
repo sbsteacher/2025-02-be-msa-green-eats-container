d7997f3271a691cee3fb1e741af9459b00cc1ea4
sbsteacher <id@gmail.com>
2026-06-15 15:50:06 +0900

    스웨거 문서 적용 완료

---
+++ document-service/src/test/java/com/green/eats/DocumentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.eats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
