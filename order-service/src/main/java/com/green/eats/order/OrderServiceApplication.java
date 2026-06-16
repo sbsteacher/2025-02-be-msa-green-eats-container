ef855051348a0c7e6749ec428fc7a929fb100a78
sbsteacher <id@gmail.com>
2026-06-16 11:48:53 +0900

    스웨거 문서 적용 다이어트2

--- order-service/src/main/java/com/green/eats/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/green/eats/order/OrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.green.eats.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class OrderServiceApplication {
     public static void main(String[] args) {
